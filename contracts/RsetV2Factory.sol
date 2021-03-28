a84edd5b9a40200e837a1f30aa874befcb65459e
rset-dev <admin_rset@protonmail.com>
2021-03-28 18:30:41 +0200

    feat: 🎸 Move to rset

--- contracts/RsetV2Factory.sol
+++ contracts/RsetV2Factory.sol
@@ -1,9 +1,9 @@
 pragma solidity =0.5.16;
 
 import './interfaces/IUniswapV2Factory.sol';
-import './UniswapV2Pair.sol';
+import './RsetV2Pair.sol';
 
-contract UniswapV2Factory is IUniswapV2Factory {
+contract RsetV2Factory is IUniswapV2Factory {
     address public feeTo;
     address public feeToSetter;
 
@@ -21,11 +21,11 @@ contract UniswapV2Factory is IUniswapV2Factory {
     }
 
     function createPair(address tokenA, address tokenB) external returns (address pair) {
-        require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');
+        require(tokenA != tokenB, 'RsetV2: IDENTICAL_ADDRESSES');
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
-        require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
-        bytes memory bytecode = type(UniswapV2Pair).creationCode;
+        require(token0 != address(0), 'RsetV2: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'RsetV2: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(RsetV2Pair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
@@ -38,12 +38,12 @@ contract UniswapV2Factory is IUniswapV2Factory {
     }
 
     function setFeeTo(address _feeTo) external {
-        require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
+        require(msg.sender == feeToSetter, 'RsetV2: FORBIDDEN');
         feeTo = _feeTo;
     }
 
     function setFeeToSetter(address _feeToSetter) external {
-        require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
+        require(msg.sender == feeToSetter, 'RsetV2: FORBIDDEN');
         feeToSetter = _feeToSetter;
     }
 }
